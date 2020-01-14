c0955ac0a72b7bbf881eb3b5055a368c2d80f4b2
Mihaly Dombi <dombimihaly89@gmail.com>
2020-01-14 19:58:44 +0100

    commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
+import com.example.demo.persistence.entity.DomesticTransaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		DomesticTransaction domesticTransaction = new DomesticTransaction();
 	}
 
 }
